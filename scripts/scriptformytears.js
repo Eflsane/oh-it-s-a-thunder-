ae17d6b84faa3f608073533773d3d8f91b5c5f4c
Eflsane <45204180+Eflsane@users.noreply.github.com>
2019-04-20 06:18:01 +0000

    Add files via upload
    .fixed watchmythunder page
    .added new buttons
    .fixed photonder and thunculture pages
    .added new animations
    .deleted iseenoevil page

--- scripts/scriptformytears.js
+++ scripts/scriptformytears.js
@@ -6,3 +6,6 @@ a = randomizer(1, 3);
 if (a === 1) alert('Разрази меня гром!');
 if (a === 2) alert('*Вас ослепило сиянием молгии');
 if (a === 3) alert('Добро пожаловать в обитель гроз');
+
+
+function okb1() { document.getElementById('advsby').style = "display: none"; }
